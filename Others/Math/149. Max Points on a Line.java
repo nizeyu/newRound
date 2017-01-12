c33b96e1b0e8c6df65d363241971458031e56367
Zeyu Ni <zeyuni827@gmail.com>
2017-01-12 14:44:45 -0500

    Create 149. Max Points on a Line.java

---
+++ Others/Math/149. Max Points on a Line.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution
+{
+    public int maxPoints(Point[] points)
+    {
+        if(points == null || points.length == 0)
+            return 0;
+        
+        HashMap<Double, Integer> map = new HashMap<Double, Integer>();
+        int max = 1;
+        
+        for(int i = 0; i < points.length; i++)
+        {
+            map.clear();
+            map.put((double)Integer.MIN_VALUE, 1);
+            
+            int dup = 0;
+            for(int j = i + 1; j < points.length; j++)
+            {
+                if(points[j].x == points[i].x && points[j].y == points[i].y)
+                {
+                    dup++;
+                    continue;
+                }
+                
+                double key = points[j].x - points[i].x == 0 ?
+                    Integer.MAX_VALUE : 
+                    0.0 + (double)(points[j].y - points[i].y) / (double)(points[j].x - points[i].x);
+                
+                if(map.containsKey(key))
+                {
+                    map.put(key, map.get(key) + 1);
+                }
+                else
+                {
+                    map.put(key, 2);
+                }
+            }
+            
+            for(int temp : map.values())
+            {
+                if(temp + dup > max)
+                    max = temp + dup;
+            }
+        }
+        return max;
+    }
+}
