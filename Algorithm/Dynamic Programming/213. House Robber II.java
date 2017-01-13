8750daab76ef25dcccb167f59a2ece01a34724a2
Zeyu Ni <zeyuni827@gmail.com>
2017-01-13 16:57:09 -0500

    Update 213. House Robber II.java

--- Algorithm/Dynamic Programming/213. House Robber II.java
+++ Algorithm/Dynamic Programming/213. House Robber II.java
@@ -13,17 +13,13 @@ public class Solution
         dp1[0] = 0;
         dp1[1] = nums[0];
         for(int i = 2; i < nums.length; i++)
-        {
             dp1[i] = Math.max(dp1[i-1], dp1[i-2] + nums[i-1]);
-        }
         
         int[] dp2 = new int[nums.length];
         dp2[0] = 0;
         dp2[1] = nums[1];
         for(int i = 2; i < nums.length; i++)
-        {
             dp2[i] = Math.max(dp2[i-1], dp2[i-2] + nums[i]);
-        }
         
         return Math.max(dp1[dp1.length - 1], dp2[dp2.length - 1]);
     }
