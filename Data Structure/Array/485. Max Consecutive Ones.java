3acd2524756d5a6d40ae961c1772451dc6efaa56
Zeyu Ni <zeyuni827@gmail.com>
2017-01-19 15:11:20 -0500

    Create 485. Max Consecutive Ones.java

---
+++ Data Structure/Array/485. Max Consecutive Ones.java
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int findMaxConsecutiveOnes(int[] nums)
+    {
+        if(nums == null || nums.length == 0)
+            return 0;
+        
+        int count = 0;
+        int result = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == 0)
+            {
+                result = Math.max(result, count);
+                count = 0;
+            }
+            else
+            {
+                count++;
+                result = Math.max(result, count);
+            }
+        }
+        return result;
+    }
+}
