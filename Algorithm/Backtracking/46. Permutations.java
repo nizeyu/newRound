51ee2056ca05ff1fe20a32ec4944c981fd48f4a4
Zeyu Ni <zeyuni827@gmail.com>
2017-01-20 18:25:09 -0500

    Create 46. Permutations.java

---
+++ Algorithm/Backtracking/46. Permutations.java
@@ -0,0 +1,29 @@
+public class Solution
+{
+    public List<List<Integer>> permute(int[] nums)
+    {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0)
+            return result;
+        dfs(nums, new ArrayList<>(), result);
+        
+        return result;
+    }
+    
+    private void dfs(int[] nums, List<Integer> permutation, List<List<Integer>> result)
+    {
+        if(permutation.size() == nums.length)
+            result.add(new ArrayList(permutation));
+        else
+        {
+            for(int i = 0; i < nums.length; i++)
+            {
+                if(permutation.contains(nums[i]))
+                    continue;
+                permutation.add(nums[i]);
+                dfs(nums, permutation, result);
+                permutation.remove(permutation.size() - 1);
+            }
+        }
+    }
+}
