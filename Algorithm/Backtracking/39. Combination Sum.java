1f2c595059b36bbce2671e0122f70cdb94b2d5a9
Zeyu Ni <zeyuni827@gmail.com>
2017-01-21 14:17:39 -0500

    Create 39. Combination Sum.java

---
+++ Algorithm/Backtracking/39. Combination Sum.java
@@ -0,0 +1,32 @@
+public class Solution
+{
+    public List<List<Integer>> combinationSum(int[] candidates, int target)
+    {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if(candidates == null || candidates.length == 0)
+            return result;
+            
+        Arrays.sort(candidates);
+        combinationSum(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+    
+    private void combinationSum(int[] candidates, int target, int start, List<Integer> tempList, List<List<Integer>> result)
+    {
+        if(target == 0)
+            result.add(new ArrayList<>(tempList));
+        else
+        {
+            for(int i = start; i < candidates.length; i++)
+            {
+                if(target < candidates[i])
+                    return;
+                tempList.add(candidates[i]);
+                combinationSum(candidates, target - candidates[i], i, tempList, result);
+                // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
