5fe912c80601646a97cdf5c93cb68146c5456e26
Zeyu Ni <zeyuni827@gmail.com>
2017-01-23 17:38:29 -0500

    Update 382. Linked List Random Node.java

--- Data Structure/Linked List/382. Linked List Random Node.java
+++ Data Structure/Linked List/382. Linked List Random Node.java
@@ -1,3 +1,4 @@
+//Method 1: naive
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -37,6 +38,47 @@ public class Solution
         return p.val;
     }
 }
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+
+
+//Method 2: https://www.hrwhisper.me/leetcode-linked-list-random-node/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    private ListNode head;
+    private Random random;
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head)
+    {
+        this.head = head;
+        random = new Random();
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom()
+    {
+        int result = 0;
+        ListNode p = head;
+        for(int i = 1; p != null; i++, p = p.next)
+            if(random.nextInt(i) == 0)
+                result = p.val;
+
+        return result;
+    }
+}
+
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(head);
