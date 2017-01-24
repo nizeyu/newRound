dabbc17349664dfcaff5b6afe44328d37d729d37
Zeyu Ni <zeyuni827@gmail.com>
2017-01-24 14:42:28 -0500

    Update 437. Path Sum III.java

--- Algorithm/DFS/437. Path Sum III.java
+++ Algorithm/DFS/437. Path Sum III.java
@@ -30,3 +30,35 @@ public class Solution
         return result;
     }
 }
+
+//Method 2: Prefix sum method
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public int pathSum(TreeNode root, int sum)
+    {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0, 1);
+        return helper(root, 0, sum, preSum);
+    }
+    private int helper(TreeNode root, int sum, int target, HashMap<Integer, Integer> preSum)
+    {
+        if(root == null)
+            return 0;
+        sum += root.val;
+        int res = preSum.getOrDefault(sum - target, 0);
+        preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        
+        res += helper(root.left, sum, target, preSum) + helper(root.right, sum, target, preSum);
+        preSum.put(sum, preSum.get(sum) - 1);
+        return res;
+    }
+}
