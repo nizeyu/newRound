e32f66ff8b1ce851a4203d560483ef224d673611
Zeyu Ni <zeyuni827@gmail.com>
2017-02-01 13:32:55 -0500

    Create 495. Teemo Attacking.java

---
+++ Data Structure/Array/495. Teemo Attacking.java
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public int findPoisonedDuration(int[] timeSeries, int duration)
+    {
+        if(timeSeries == null || timeSeries.length == 0 || duration == 0)
+            return 0;
+        
+        int result = 0;
+        int end = timeSeries[0];
+        for(int i = 0; i < timeSeries.length; i++)
+        {
+            if(i > 0 && timeSeries[i] < end)
+            {
+                result += duration - (end - timeSeries[i]);
+            }
+            else
+            {
+                result += duration;
+            }
+            end = timeSeries[i] + duration;
+        }
+        return result;
+    }
+}
