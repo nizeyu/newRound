0e00defa3c73b1b1b4e262597f92fe6c5275720b
Zeyu Ni <zeyuni827@gmail.com>
2017-02-08 17:44:10 -0500

    Create 122. Best Time to Buy and Sell Stock II.java

---
+++ Algorithm/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,42 @@
+//My Solution
+//Method 1:
+public class Solution
+{
+    public int maxProfit(int[] prices)
+    {
+        if(prices == null || prices.length <= 1)
+            return 0;
+        
+        int result = 0;
+        
+        for(int i = 1; i < prices.length; i++)
+        {
+            if(prices[i] > prices[i-1])
+                result += prices[i] - prices[i-1];
+        }
+        return result;
+    }
+}
+
+//Method 2:
+public class Solution
+{
+    public int maxProfit(int[] prices)
+    {
+        if(prices == null || prices.length <= 1)
+            return 0;
+        
+        int[] buy = new int[prices.length];
+        int[] sell = new int[prices.length];
+        buy[0] = -prices[0];
+        buy[1] = Math.max(buy[0], -prices[1]);
+        sell[0] = 0;
+        sell[1] = Math.max(0, prices[1] - prices[0]);
+        for(int i = 2; i < prices.length; i++)
+        {
+            buy[i] = Math.max(buy[i-1], sell[i-1] - prices[i]);
+            sell[i] = Math.max(sell[i-1], buy[i-1] + prices[i]);
+        }
+        return Math.max(sell[prices.length-1], sell[prices.length-2]);
+    }
+}
