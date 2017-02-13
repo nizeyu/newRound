6ec6b62ba2eb653a414cb927bd4192c3812dba7f
Zeyu Ni <zeyuni827@gmail.com>
2017-02-13 10:07:04 -0500

    Create 199. Binary Tree Right Side View.java

---
+++ Algorithm/DFS/199. Binary Tree Right Side View.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public List<Integer> rightSideView(TreeNode root)
+    {
+        List<Integer> result = new ArrayList<>();
+        
+        if(root == null)
+            return result;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i = 0; i < size; i++)
+            {
+                TreeNode node = q.poll();
+                
+                if(i == 0)
+                    result.add(node.val);
+                
+                if(node.right != null)
+                    q.offer(node.right);
+                if(node.left != null)
+                    q.offer(node.left);
+            }
+        }
+        return result;
+    }
+}
