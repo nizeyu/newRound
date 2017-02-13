02645ff844b9fdbc350a16c3a29530a1069e72fd
Zeyu Ni <zeyuni827@gmail.com>
2017-02-13 09:35:13 -0500

    Create 111. Minimum Depth of Binary Tree.java

---
+++ Algorithm/DFS/111. Minimum Depth of Binary Tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public int minDepth(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        Queue<Integer> depth = new LinkedList<>();
+        depth.offer(1);
+        
+        while(!q.isEmpty())
+        {
+            TreeNode node = q.poll();
+            int d = depth.poll();
+            
+            if(node.left == null && node.right == null)
+                return d;
+                
+            if(node.left != null)
+            {
+                q.offer(node.left);
+                depth.offer(d + 1);
+            }
+                
+            if(node.right != null)
+            {
+                q.offer(node.right);
+                depth.offer(d + 1);
+            }
+                
+        }
+        return 1;
+    }
+}
