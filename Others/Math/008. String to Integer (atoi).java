76d89faf7fb4d816af072399f0ef3d87aaa562bf
Zeyu Ni <zeyuni827@gmail.com>
2017-02-14 08:04:13 -0500

    Create 008. String to Integer (atoi).java

---
+++ Others/Math/008. String to Integer (atoi).java
@@ -0,0 +1,41 @@
+public class Solution
+{
+    static final int max = Integer.MAX_VALUE / 10;
+    
+    public int myAtoi(String str)
+    {
+        if(str == null || str.length() == 0)
+            return 0;
+        
+        int i = 0;
+        int l = str.length();
+        
+        while(i < l && Character.isWhitespace(str.charAt(i))) i++;
+        
+        int sign = 1;
+        
+        if(i < l && str.charAt(i) == '+')
+            i++;
+        else if(i < l && str.charAt(i) == '-')
+        {
+            sign = -1;
+            i++;
+        }
+        
+        int digits = 0;
+        
+        while(i < l && Character.isDigit(str.charAt(i)))
+        {
+            int digit = Character.getNumericValue(str.charAt(i));
+            
+            if(digits > max || digits == max && digit >= 8)
+                return sign == 1 ? Integer.MAX_VALUE:Integer.MIN_VALUE;
+                
+            digits = digits * 10 + digit;
+            
+            i++;
+        }
+        
+        return sign * digits;
+    }
+}
