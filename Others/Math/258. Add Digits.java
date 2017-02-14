9779d7da0221b40a388ed7829e66fded4315b842
Zeyu Ni <zeyuni827@gmail.com>
2017-02-14 07:56:56 -0500

    Create 258. Add Digits.java

---
+++ Others/Math/258. Add Digits.java
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while(num != 0){
+            int t = num % 10;
+            sum = sum + t;
+            num = num/10;
+        }
+        if(sum < 10){
+            return sum;
+        }else{
+            addDigits(sum);
+        }
+        
+    }
+};
