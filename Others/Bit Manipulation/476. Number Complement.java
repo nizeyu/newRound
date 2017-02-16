bb3aec05c2626944c5635f1f841c0a3e4190f05f
Zeyu Ni <zeyuni827@gmail.com>
2017-02-16 15:04:09 -0500

    Update 476. Number Complement.java

--- Others/Bit Manipulation/476. Number Complement.java
+++ Others/Bit Manipulation/476. Number Complement.java
@@ -1,25 +1,23 @@
-Method 1
-public class Solution
-{
-    public int findComplement(int num)
-    {
-        int mask = 1, temp = num;
-        while(temp > 0)
-        {
-            mask = mask << 1;
-            temp = temp >> 1;
-        }
-        return num^(mask-1);
-    }
-}
-
-Method 2
-public class Solution
-{
-    public int findComplement(int num)
-    {
-        int mask = (Integer.highestOneBit(num) << 1) - 1;
+//My Solution
+public class Solution {
+    public int findComplement(int num) {
+        int helper1 = 1;//to indicate the first "1"
         
-        return num^mask;
+        for(int i = 0; i < 30; i++) {
+            helper1 <<= 1;
+        }
+        while((num & helper1) == 0 && helper1 >= 0)
+            helper1 >>= 1;
+        
+        int helper2 = 1;//to flip the bit
+        for(int i = 0; i < 31; i++) {
+            num ^= helper2;
+            System.out.println(Integer.toBinaryString(num));
+            if(helper2 < helper1)
+                helper2 <<= 1;
+            else 
+                break;
+        }
+        return num;
     }
 }
