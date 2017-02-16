989fb310cc929e14855264d5f778a7d01f892d63
Zeyu Ni <zeyuni827@gmail.com>
2017-02-16 15:14:24 -0500

    Update 476. Number Complement.java

--- Others/Bit Manipulation/476. Number Complement.java
+++ Others/Bit Manipulation/476. Number Complement.java
@@ -2,11 +2,9 @@
 public class Solution {
     public int findComplement(int num) {
         int helper1 = 1;//to indicate the first "1"
+        helper1 <<= 30;
         
-        for(int i = 0; i < 30; i++) {
-            helper1 <<= 1;
-        }
-        while((num & helper1) == 0 && helper1 >= 0)
+        while((num & helper1) == 0)
             helper1 >>= 1;
         
         int helper2 = 1;//to flip the bit
