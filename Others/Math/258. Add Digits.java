3c9fddc4dd2cf7c226b427564c007c03c95e49d0
Zeyu Ni <zeyuni827@gmail.com>
2017-02-19 09:53:02 -0500

    Update 258. Add Digits.java

--- Others/Math/258. Add Digits.java
+++ Others/Math/258. Add Digits.java
@@ -1,3 +1,17 @@
+//Math: https://zh.wikipedia.org/wiki/%E6%95%B8%E6%A0%B9
+public class Solution {
+    public int addDigits(int num) {
+        if(num == 0)
+            return 0;
+            
+        if(num % 9 == 0)
+            return 9;
+        else
+            return num % 9;
+    }
+}
+
+//My Solution
 class Solution {
 public:
     int addDigits(int num) {
