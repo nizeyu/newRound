7d57465dffac287a192b5efd52553b65e9d492c6
Zeyu Ni <zeyuni827@gmail.com>
2017-02-19 09:34:48 -0500

    Update 389. Find the Difference.java

--- Others/Bit Manipulation/389. Find the Difference.java
+++ Others/Bit Manipulation/389. Find the Difference.java
@@ -1,14 +1,16 @@
-public class Solution
-{
-    public char findTheDifference(String s, String t)
-    {
-        char c = 0;
-        for(int i = 0; i < s.length(); i++)
-            c ^= s.charAt(i);
+//My Solution
+public class Solution {
+    public char findTheDifference(String s, String t) {
         
-        for(int j = 0; j < t.length(); j++)
-            c ^= t.charAt(j);
+        char res = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            res ^= s.charAt(i);
+        }
         
-        return c;
+        for(int i = 0; i < t.length(); ++i) {
+            res ^= t.charAt(i);
+        }
+        
+        return res;
     }
 }
