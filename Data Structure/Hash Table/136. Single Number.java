5ddc45dd5e6bc5bf249db99f9e5b624a739ec36b
Zeyu Ni <zeyuni827@gmail.com>
2017-02-18 20:03:59 -0500

    Update 136. Single Number.java

--- Data Structure/Hash Table/136. Single Number.java
+++ Data Structure/Hash Table/136. Single Number.java
@@ -1,15 +1,25 @@
-public class Solution
-{
-    public int singleNumber(int[] nums)
-    {
-        if(nums == null || nums.length == 0)
-            throw new IllegalArgumentException("Input is invalid");
-        
-        int x = 0;
-        
-        for(int num: nums)
-            x ^= num;
-        
-        return x;
+//My Solution 1: XOR
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int i = 0; i < nums.length; i++)
+            res ^= nums[i];
+        return res;
+    }
+}
+
+//My Solution 2: HashSet
+public class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(!set.contains(num))
+                set.add(num);
+            else
+                set.remove(num);
+        }
+        Iterator i = set.iterator();
+        int res = (int)i.next();
+        return res;
     }
 }
