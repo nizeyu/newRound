628ed5109b1afc6002048003b3715b765bedf705
Zeyu Ni <zeyuni827@gmail.com>
2017-02-19 10:50:13 -0500

    Update 226. Invert Binary Tree.java

--- Algorithm/BFS/226. Invert Binary Tree.java
+++ Algorithm/BFS/226. Invert Binary Tree.java
@@ -1,3 +1,36 @@
+//My Solution: Recursive
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        
+        invert(root);
+        return root;
+    }
+    
+    private void invert(TreeNode node) {
+        if(node == null)
+            return;
+        
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        
+        invertTree(node.left);
+        invertTree(node.right);
+    }
+}
+
+//BFS
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
