67b24779dd2c64b3412489d4b4708781f08ab5c7
Zeyu Ni <zeyuni827@gmail.com>
2017-02-21 17:08:13 -0500

    Update 217. Contains Duplicate.java

--- Data Structure/Hash Table/217. Contains Duplicate.java
+++ Data Structure/Hash Table/217. Contains Duplicate.java
@@ -1,18 +1,14 @@
-//Method 1: 
-public class Solution
-{
-    public boolean containsDuplicate(int[] nums)
-    {
-        if (nums == null && nums.length == 0)
+//My Solution: 
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0)
             return false;
-            
-        HashSet<Integer> set = new HashSet<>();
         
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(set.contains(nums[i]))
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(set.contains(num))
                 return true;
-            set.add(nums[i]);
+            set.add(num);
         }
         return false;
     }
