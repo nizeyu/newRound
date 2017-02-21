844f68d15e5b4e691565ad72ff860360365c120e
Zeyu Ni <zeyuni827@gmail.com>
2017-02-21 08:18:45 -0500

    Update 404. Sum of Left Leaves.java

--- Data Structure/Tree/404. Sum of Left Leaves.java
+++ Data Structure/Tree/404. Sum of Left Leaves.java
@@ -1,4 +1,4 @@
-//My Solution
+//My Solution：DFS
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -37,6 +37,38 @@ public class Solution
                 else
                     stack.push(node.left);
             }
+        }
+        return sum;
+    }
+}
+
+//My Solution: Recursive
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null)
+            return 0;
+        
+        int sum = 0;
+        if(root.left != null) {
+            if(root.left.left == null && root.left.right == null)
+                sum += root.left.val;
+            else 
+                sum += sumOfLeftLeaves(root.left);
+        }
+        
+        if(root.right != null) {
+            if(root.right.left != null || root.right.right != null)
+                sum += sumOfLeftLeaves(root.right);
         }
         return sum;
     }
