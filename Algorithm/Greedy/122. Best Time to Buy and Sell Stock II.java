92a9e6f079b1f02a3d7263881206a4a70fdf39b7
Zeyu Ni <zeyuni827@gmail.com>
2017-02-21 09:46:15 -0500

    Create 122. Best Time to Buy and Sell Stock II.java

---
+++ Algorithm/Greedy/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,14 @@
+//My Solution
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0)
+            return 0;
+        
+        int res = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i-1] < prices[i])
+                res += prices[i] - prices[i-1];
+        }
+        return res;
+    }
+}
