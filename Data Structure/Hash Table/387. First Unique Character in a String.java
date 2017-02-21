fe870ed1e91ada13914ddd6b2dd50b39b3f4d4f2
Zeyu Ni <zeyuni827@gmail.com>
2017-02-21 10:24:18 -0500

    Update 387. First Unique Character in a String.java

--- Data Structure/Hash Table/387. First Unique Character in a String.java
+++ Data Structure/Hash Table/387. First Unique Character in a String.java
@@ -27,6 +27,36 @@ public class Solution
     }
 }
 
+//My Solution: 人工hash
+public class Solution {
+    public int firstUniqChar(String s) {
+        if(s == null || s.length() == 0)
+            return -1;
+        
+        int[] x = new int[26];
+        int min = Integer.MAX_VALUE;
+        
+        for(int i  = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            if(x[index] > 0)
+                x[index] = Integer.MAX_VALUE;
+            else
+                x[index] = i+1;
+        }
+        
+        for(int i  = 0; i < 26; i++) {
+            if(x[i] > 0)
+                min = Math.min(min, x[i]);
+        }
+            
+        if(min == Integer.MAX_VALUE)
+            return -1;
+            
+        return min - 1;
+    }
+}
+
+//discuss 简化
 public class Solution {
     public int firstUniqChar(String s) {
         int freq [] = new int[26];
