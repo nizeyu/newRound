4176d39694760c8a8a2284ed96f28aa96f34ca1d
Zeyu Ni <zeyuni827@gmail.com>
2017-02-21 16:57:33 -0500

    Update 409. Longest Palindrome.java

--- Data Structure/Hash Table/409. Longest Palindrome.java
+++ Data Structure/Hash Table/409. Longest Palindrome.java
@@ -1,3 +1,36 @@
+//My Solution: 人工hash
+public class Solution {
+    public int longestPalindrome(String s) {
+        if(s == null || s.length() == 0)
+            return 0;
+        
+        int[] map = new int[256];
+        for(int i = 0; i < s.length(); ++i)
+            map[s.charAt(i)]++;
+        
+        int res = 0;
+        boolean flag = false;//for odd element exit 
+        for(int i = 0; i < 256; ++i) {
+            if(map[i] == 1)
+                flag = true;
+                
+            if(map[i] > 1) {
+                if(map[i] % 2 == 0)
+                    res += map[i];
+                else {
+                    res += map[i] - 1;
+                    flag = true;
+                }
+            }
+        }
+        if(flag)
+            return res + 1;
+        else
+            return res;
+    }
+}
+
+//HashSet
 public class Solution
 {
     public int longestPalindrome(String s)
