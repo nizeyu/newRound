5c0ff08263750ebbca0f8641c615abaf058e08c3
Zeyu Ni <zeyuni827@gmail.com>
2017-02-22 11:46:04 -0500

    Update 447. Number of Boomerangs.java

--- Data Structure/Hash Table/447. Number of Boomerangs.java
+++ Data Structure/Hash Table/447. Number of Boomerangs.java
@@ -1,34 +1,32 @@
-public class Solution
-{
-    public int numberOfBoomerangs(int[][] points)
-    {
+public class Solution {
+    public int numberOfBoomerangs(int[][] points) {
         if(points == null || points.length == 0)
             return 0;
         
-        int result = 0;
+        int res = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < points.length; i++)
-        {
-            for(int j = 0; j < points.length; j++)
-            {
+        for(int i = 0; i < points.length; ++i) {
+            for(int j = 0; j < points.length; ++j) {
                 if(i == j)
                     continue;
+                
                 int d = getDistance(points[i], points[j]);
                 map.put(d, map.getOrDefault(d, 0) + 1);
             }
             
-            for(int val : map.values())
-                result += val * (val - 1);
+            for(int d : map.values()) {
+                res += d * (d - 1);
+            }
             
             map.clear();
         }
-        return result;
+        return res;
     }
     
-    private int getDistance(int[] x, int[] y)
-    {
-        int dx = x[0] - y[0];
-        int dy = x[1] - y[1];
-        return dx*dx + dy*dy;
+    private int getDistance(int[] point1, int[] point2) {
+        return (int)(Math.pow(point1[0] - point2[0], 2) + Math.pow(point1[1] - point2[1], 2));
     }
 }
+
+//Time complexity:  O(n^2)
+//Space complexity: O(n)
