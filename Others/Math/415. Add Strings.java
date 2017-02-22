35554e1fe349a6e2d5a3ab9cd80efc821ba76ae7
Zeyu Ni <zeyuni827@gmail.com>
2017-02-22 14:44:30 -0500

    Update 415. Add Strings.java

--- Others/Math/415. Add Strings.java
+++ Others/Math/415. Add Strings.java
@@ -1,3 +1,41 @@
+//My Solution: 2刷
+public class Solution {
+    public String addStrings(String num1, String num2) {
+        if(num1 == null || num1.length() == 0) return num2;
+        if(num2 == null || num2.length() == 0) return num1;
+        
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0) {
+            int x = i >= 0 ? Character.getNumericValue(num1.charAt(i)) : 0;
+            int y = j >= 0 ? Character.getNumericValue(num2.charAt(j)) : 0;
+            int digit = x + y + carry;
+            carry = digit / 10;
+            
+            sb.insert(0, digit % 10);
+            i--;
+            j--;
+        }
+        
+        while(i >= 0) {
+            int digit = Character.getNumericValue(num1.charAt(i)) + carry;
+            sb.insert(0, digit % 10);
+            carry = digit / 10;
+        }
+        while(j >= 0) {
+            int digit = Character.getNumericValue(num1.charAt(j)) + carry;
+            sb.insert(0, digit % 10);
+            carry = digit / 10;
+        }
+        if(carry != 0) sb.insert(0, carry);
+        
+        return sb.toString();
+    }
+}
+
+//My Solution: 1刷
 public class Solution
 {
     public String addStrings(String num1, String num2)
