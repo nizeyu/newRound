988a670d4748e1b5534b5bcd6ec221c6e6802a1f
Zeyu Ni <zeyuni827@gmail.com>
2017-02-22 16:41:20 -0500

    Update 231. Power of Two.java

--- Others/Math/231. Power of Two.java
+++ Others/Math/231. Power of Two.java
@@ -1,3 +1,14 @@
+//My Solution: bit manipulate
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n < 1)
+            return false;
+        return Integer.bitCount(n) == 1;
+        
+    }
+}
+
+//My Solution: while
 public class Solution
 {
     public boolean isPowerOfTwo(int n)
