6d7383c9cec8b4f8827756ca9194cfedb7591c75
Zeyu Ni <zeyuni827@gmail.com>
2017-02-24 10:17:59 -0500

    Update 459. Repeated Substring Pattern.java

--- Data Structure/String/459. Repeated Substring Pattern.java
+++ Data Structure/String/459. Repeated Substring Pattern.java
@@ -1,19 +1,15 @@
-public class Solution
-{
-    public boolean repeatedSubstringPattern(String str)
-    {
-        int l = str.length();
-        for(int i = l / 2; i >= 1; i--)
-        {
-            if(l % i == 0)
-            {
-                int m = l / i;
-                String subS = str.substring(0, i);
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+        for(int i = len / 2; i >= 1; i--) {
+            if(len % i == 0) {
+                String sub = s.substring(0, i);
                 StringBuilder sb = new StringBuilder();
-                for(int j = 0; j < m; j++)
-                    sb.append(subS);
-                if(sb.toString().equals(str))
+                for(int j = 0; j < len / i; j++) 
+                    sb.append(sub);
+                if(sb.toString().equals(s)) {
                     return true;
+                }
             }
         }
         return false;
