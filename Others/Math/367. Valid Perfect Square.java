830615d7b3f0113fa5f5fbc57bd3a3bbd833edf3
Zeyu Ni <zeyuni827@gmail.com>
2017-02-24 15:48:29 -0500

    Update 367. Valid Perfect Square.java

--- Others/Math/367. Valid Perfect Square.java
+++ Others/Math/367. Valid Perfect Square.java
@@ -1,3 +1,29 @@
+//Binary Search
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num == 1)
+            return true;
+        
+        long l = 1;
+        long r = num / 2;
+        long nums = (long)num;
+        while(l <= r) {
+            long mid = l + (r - l) / 2;
+            long sqrt = mid * mid;
+            
+            if(sqrt == nums) {
+                return true;
+            } else if(sqrt < nums) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+
+//Math
 public class Solution
 {
     public boolean isPerfectSquare(int num)
