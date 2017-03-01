57a685334eedbfe0e90382ecd0818c77e4257bd5
Zeyu Ni <zeyuni827@gmail.com>
2017-03-01 15:07:46 -0500

    Update 374. Guess Number Higher or Lower.java

--- Algorithm/Binary Search/374. Guess Number Higher or Lower.java
+++ Algorithm/Binary Search/374. Guess Number Higher or Lower.java
@@ -1,4 +1,26 @@
-//My Solution
+//My Solution: new
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int l = 1;
+        int r = n;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            
+            if(guess(mid) <= 0)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+}
+
+//My Solution: old
 /* The guess API is defined in the parent class GuessGame.
    @param num, your guess
    @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
