d83f2c8de009188eae4efba9757cb32e041ce7dc
Zeyu Ni <zeyuni827@gmail.com>
2017-02-28 21:29:14 -0500

    Update 009. Palindrome Number.java

--- Others/Math/009. Palindrome Number.java
+++ Others/Math/009. Palindrome Number.java
@@ -1,3 +1,4 @@
+//My Solution
 public class Solution
 {
     public boolean isPalindrome(int x)
@@ -20,5 +21,21 @@ public class Solution
         }
         
         return true;
+    }
+}
+
+//My Solution:照镜子
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        
+        int n = x;
+        int res = 0;
+        while(n > 0) {
+            res = res * 10 + n % 10;
+            n /= 10;
+        }
+        return res == x;
     }
 }
