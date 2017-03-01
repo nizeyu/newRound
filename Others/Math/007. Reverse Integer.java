e13bf49df48e7d4c79f9b3627fa1dada3f1da3c5
Zeyu Ni <zeyuni827@gmail.com>
2017-02-28 21:42:21 -0500

    Update 007. Reverse Integer.java

--- Others/Math/007. Reverse Integer.java
+++ Others/Math/007. Reverse Integer.java
@@ -1,3 +1,26 @@
+//My Soluiton
+public class Solution {
+    public int reverse(int x) {
+        boolean flag = false;
+        if(x < 0)
+            flag = true;
+        x = Math.abs(x);
+        
+        long res = 0;
+        while(x > 0) {
+            res = res * 10 + x % 10;
+            if(res > Integer.MAX_VALUE)
+                return 0;
+            x /= 10;
+        }
+        if(flag)
+            return -(int)res;
+        else
+            return (int)res;
+    }
+}
+
+
 public class Solution
 {
     public int reverse(int x)
