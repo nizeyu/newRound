3123dd432947293f3dab98fa94ff4c5731688de1
Zeyu Ni <zeyuni827@gmail.com>
2017-03-02 17:50:32 -0500

    Update 219. Contains Duplicate II.java

--- Data Structure/Hash Table/219. Contains Duplicate II.java
+++ Data Structure/Hash Table/219. Contains Duplicate II.java
@@ -1,3 +1,22 @@
+//My Solution
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums == null || nums.length == 0)
+            return false;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                if(i - map.get(nums[i]) <= k)
+                    return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
+
+//My Solution
 public class Solution
 {
     public boolean containsNearbyDuplicate(int[] nums, int k)
