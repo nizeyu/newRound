5a9edefb48ff4bfb64ba54814c7aa79b8dac3fe4
Zeyu Ni <zeyuni827@gmail.com>
2017-03-02 21:00:36 -0500

    Update 058. Length of Last Word.java

--- Data Structure/String/058. Length of Last Word.java
+++ Data Structure/String/058. Length of Last Word.java
@@ -10,3 +10,27 @@ public class Solution
         return s.length() - lastIndex;
     }
 }
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s == null || s.length() == 0)
+            return 0;
+        
+        int count = 0;
+        int index = s.length() - 1;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) == ' ')
+                index--;
+            else
+                break;
+        }
+        
+        for(int i = index; i >= 0; i--) {
+            if(s.charAt(i) != ' ')
+                count++;
+            else
+                break;
+        }
+        return count;
+    }
+}
