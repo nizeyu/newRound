ad3303dd2b391cb5ea2a2c5e4b6ed2b2e65e9a18
Zeyu Ni <zeyuni827@gmail.com>
2017-03-03 11:07:31 -0500

    Update 067. Add Binary.java

--- Others/Math/067. Add Binary.java
+++ Others/Math/067. Add Binary.java
@@ -1,50 +1,26 @@
-public class Solution
-{
-    public String addBinary(String a, String b)
-    {
-        if(a == null || a.length() == 0)
-            return b;
-        if(b == null || b.length() == 0)
-            return a;
-        
-        Stack<Character> stackA = new Stack<>();
-        Stack<Character> stackB = new Stack<>();
-        
-        for(int i = 0; i < a.length(); i++)
-            stackA.push(a.charAt(i));
-        for(int i = 0; i < b.length(); i++)
-            stackB.push(b.charAt(i));
+//My Solution
+public class Solution {
+    public String addBinary(String a, String b) {
+        if(a == null || a.length() == 0) return b;
+        if(b == null || b.length() == 0) return a;
         
         StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int carry = 0;
-        while(!stackA.isEmpty() || !stackB.isEmpty())
-        {
-            int x = !stackA.isEmpty() ? Character.getNumericValue(stackA.pop()) : 0;
-            int y = !stackB.isEmpty() ? Character.getNumericValue(stackB.pop()) : 0;
-            
+        while(i >= 0 || j >= 0) {
+            int x = i >= 0 ? a.charAt(i) - '0' : 0;
+            int y = j >= 0 ? b.charAt(j) - '0' : 0;
             int digit = x + y + carry;
-            
             carry = digit / 2;
             
-            sb.insert(0, Integer.toString(digit % 2));
+            sb.insert(0, digit % 2);
+            i--;
+            j--;
         }
         
-        while(!stackA.isEmpty())
-        {
-            int temp = Character.getNumericValue(stackA.pop()) + carry;
-            carry = temp / 2;
-            sb.insert(0, Integer.toString(temp % 2));
-        }
-        
-        while(!stackA.isEmpty())
-        {
-            int temp = Character.getNumericValue(stackB.pop()) + carry;
-            carry = temp / 2;
-            sb.insert(0, Integer.toString(temp % 2));
-        }
-        
-        if(carry == 1)
-             sb.insert(0, '1');
+        if(carry != 0)
+            sb.insert(0, carry);
         
         return sb.toString();
     }
