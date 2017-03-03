51b3415d4051f2f6aa0b7017fd02692cd24bf51c
Zeyu Ni <zeyuni827@gmail.com>
2017-03-03 17:14:08 -0500

    Update 028. Implement strStr().java

--- Others/Two Pointers/028. Implement strStr().java
+++ Others/Two Pointers/028. Implement strStr().java
@@ -25,3 +25,16 @@ public class Solution {
         return - 1;
     }
 }
+
+//Discuss
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0; ;i++) {
+            for(int j = 0; ;j++) {
+                if(j == needle.length()) return i;
+                if(i + j == haystack.length()) return -1;
+                if(needle.charAt(j) != haystack.charAt(i+j)) break;
+            }
+        }
+    }
+}
