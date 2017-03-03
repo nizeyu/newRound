5ecf20e13487a6ba2707059eb49b0084351efd8a
Zeyu Ni <zeyuni827@gmail.com>
2017-03-03 16:43:05 -0500

    Create 028. Implement strStr().java

---
+++ Others/Two Pointers/028. Implement strStr().java
@@ -0,0 +1,27 @@
+//My Soluton
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.length() == 0 && needle.length() == 0)
+            return 0;
+        else if(haystack.length() == 0)
+            return -1;
+        else if(needle.length() == 0)
+            return 0;
+        
+        int j = 0;
+        for(int i = 0; i < haystack.length(); i++) {
+            if(haystack.length() - i + 1 < needle.length())
+                return -1;
+                
+            int k = i;
+            while(k < haystack.length() && haystack.charAt(k) == needle.charAt(j)) {
+                if(j == needle.length() - 1)
+                    return k - needle.length() + 1;
+                k++;
+                j++;
+            }
+            j = 0;
+        }
+        return - 1;
+    }
+}
