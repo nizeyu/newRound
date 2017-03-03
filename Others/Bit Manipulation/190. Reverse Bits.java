c82210c8741112ad66d334299194671c1bad2ae1
Zeyu Ni <zeyuni827@gmail.com>
2017-03-03 15:59:45 -0500

    Update 190. Reverse Bits.java

--- Others/Bit Manipulation/190. Reverse Bits.java
+++ Others/Bit Manipulation/190. Reverse Bits.java
@@ -1,16 +1,11 @@
-public class Solution
-{
+public class Solution {
     // you need treat n as an unsigned value
-    public int reverseBits(int n)
-    {
-        int result = 0;
-        
-        for(int i = 0; i < 32; i++)
-        {
-            result |= (n & 1) << (31 - i);
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            res |= (n & 1) << (31 - i); 
             n >>>= 1;
         }
-        
-        return result;
+        return res;
     }
 }
