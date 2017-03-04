94b2e0ffcc468f0df604230014bd06f9e3f22cc4
Zeyu Ni <zeyuni827@gmail.com>
2017-03-04 10:58:57 -0500

    Update 013. Roman to Integer.java

--- Others/Math/013. Roman to Integer.java
+++ Others/Math/013. Roman to Integer.java
@@ -1,31 +1,38 @@
-public class Solution
-{
-    public int romanToInt(String s)
-    {
-        if(s == null || s.length() == 0)
-            return 0;
-        
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('M', 1000);
-        map.put('D', 500);
-        map.put('C', 100);
-        map.put('L', 50);
-        map.put('X', 10);
-        map.put('V', 5);
-        map.put('I', 1);
-        
-        int result = 0;
-        int previous = 0;
-        
-        for(int i = 0; i < s.length(); i++)
-        {
-            int current = map.get(s.charAt(i));
-            
-            result += current > previous ? (current - 2 * previous):current;
-            
-            previous = current;
+public class Solution {
+    public int romanToInt(String s) {
+        int nums[] = new int[s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            switch(s.charAt(i)) {
+                case 'M':
+                    nums[i] = 1000;
+                    break;
+                case 'D':
+                    nums[i] = 500;
+                    break;
+                case 'C':
+                    nums[i] = 100;
+                    break;
+                case 'L':
+                    nums[i] = 50;
+                    break;
+                case 'X':
+                    nums[i] = 10;
+                    break;
+                case 'V':
+                    nums[i] = 5;
+                    break;
+                case 'I':
+                    nums[i] = 1;
+                    break;
+            }
         }
-        
-        return result;
+        int sum = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] < nums[i+1])
+                sum -= nums[i];
+            else
+                sum += nums[i];
+        }
+        return sum + nums[nums.length - 1];
     }
 }
