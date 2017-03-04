6fbc4707cc80db1fa969b164a7536011256d6258
Zeyu Ni <zeyuni827@gmail.com>
2017-03-03 19:50:27 -0500

    Update 414. Third Maximum Number.java

--- Data Structure/Array/414. Third Maximum Number.java
+++ Data Structure/Array/414. Third Maximum Number.java
@@ -1,35 +1,26 @@
-public class Solution
-{
-    public int thirdMax(int[] nums)
-    {
+//My Solution
+public class Solution {
+    public int thirdMax(int[] nums) {
         long[] max = new long[3];
         for(int i = 0; i < 3; i++)
             max[i] = Long.MIN_VALUE;
         
-        
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(nums[i] > max[2])
-            {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > max[2]) {
                 max[0] = max[1];
                 max[1] = max[2];
                 max[2] = nums[i];
-            }
-            else if(nums[i] < max[2] && nums[i] > max[1])
-            {
+            } else if(max[1] < nums[i] && nums[i] < max[2]) {
                 max[0] = max[1];
                 max[1] = nums[i];
-            }
-            else if(nums[i] < max[1] && nums[i] >= max[0])
-            {
+            } else if(max[0] < nums[i] && nums[i] < max[1]) {
                 max[0] = nums[i];
             }
-            else
-                continue;
         }
-        if( max[0] == Long.MIN_VALUE)
-            return (int)max[2];
         
+        if(max[0] == Long.MIN_VALUE)
+            return (int)max[2];
+            
         return (int)max[0];
     }
 }
