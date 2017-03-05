86a7833d1263c1e3eaf9794cccc200faa6c85ca2
Zeyu Ni <zeyuni827@gmail.com>
2017-03-04 21:09:18 -0500

    Update 204. Count Primes.java

--- Others/Math/204. Count Primes.java
+++ Others/Math/204. Count Primes.java
@@ -1,28 +1,21 @@
-public class Solution
-{
-    public int countPrimes(int n)
-    {
-        if(n < 3)
+//My Solution
+public class Solution {
+    public int countPrimes(int n) {
+        if(n < 2)
             return 0;
-        
+            
         int count = 0;
+        boolean[] isPrime = new boolean[n];
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(isPrime[i] == false)
+                for(int j = i; j * i < n; j++)
+                    isPrime[j * i] = true;
+        }
         
-        boolean[] isPrime = new boolean[n]; //false indicate "is prime"
-
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (isPrime[i] == false) {
-				for (int j = i; j * i < n; j++) {
-					isPrime[j * i] = true;
-				}
-			}
-		}
-		
-		for(int i = 2; i < n; i++)
-		{
-		    if(isPrime[i] == false)
-		        count++;
-		}
-        
+        for(int i = 2; i < n; i++) {
+            if(!isPrime[i])
+                count++;
+        }
         return count;
     }
 }
