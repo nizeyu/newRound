f83d0a492ed2639b16a58f5d5b585456205a6816
Zeyu Ni <zeyuni827@gmail.com>
2017-03-05 17:12:50 -0500

    Update 441. Arranging Coins.java

--- Others/Math/441. Arranging Coins.java
+++ Others/Math/441. Arranging Coins.java
@@ -1,3 +1,12 @@
+//Math
+public class Solution {
+    public int arrangeCoins(int n) {
+        double N = n;
+        return (int)Math.floor(-0.5 + 0.5 * Math.sqrt(1.0 + 8.0 * N));
+    }
+}
+
+
 public class Solution
 {
     public int arrangeCoins(int n)
