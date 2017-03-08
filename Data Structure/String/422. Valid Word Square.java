81c023ae9a77eabd5373538c2404dd69492525eb
Zeyu Ni <zeyuni827@gmail.com>
2017-03-07 22:04:58 -0500

    Create 422. Valid Word Square.java

---
+++ Data Structure/String/422. Valid Word Square.java
@@ -0,0 +1,12 @@
+//重做
+public class Solution {
+    public boolean validWordSquare(List<String> words) {
+        for(int i = 0; i < words.size(); i++) {
+            for(int j = 0; j < words.get(i).length(); j++) {
+                if(j >= words.size() || words.get(j).length() <= i || words.get(i).charAt(j) != words.get(j).charAt(i))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
