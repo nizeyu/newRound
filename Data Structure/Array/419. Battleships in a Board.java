022ab62754256e5cd955916eeec9bc3732d06680
Zeyu Ni <zeyuni827@gmail.com>
2017-03-08 21:38:46 -0500

    Update 419. Battleships in a Board.java

--- Data Structure/Array/419. Battleships in a Board.java
+++ Data Structure/Array/419. Battleships in a Board.java
@@ -1,16 +1,11 @@
-public class Solution
-{
-    public int countBattleships(char[][] board)
-    {
-        int m = board.length;
-        if(m == 0) return 0;
-        int n = board[0].length;
-        int count = 0;
+public class Solution {
+    public int countBattleships(char[][] board) {
+        if(board == null || board.length == 0)
+            return 0;
         
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
+        int count = 0;
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
                 if(board[i][j] == '.') continue;
                 if(i > 0 && board[i-1][j] == 'X') continue;
                 if(j > 0 && board[i][j-1] == 'X') continue;
