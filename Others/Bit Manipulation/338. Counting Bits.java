1d924f3e047d58785397c3797f5c02d019c9085e
Zeyu Ni <zeyuni827@gmail.com>
2017-03-09 21:04:53 -0500

    Update 338. Counting Bits.java

--- Others/Bit Manipulation/338. Counting Bits.java
+++ Others/Bit Manipulation/338. Counting Bits.java
@@ -1,3 +1,18 @@
+//DP
+public class Solution {
+    public int[] countBits(int num) {
+        int[] dp = new int[num + 1];
+        int offset = 1;
+        for(int index = 1; index < num + 1; ++index) {
+            if(offset * 2 == index)
+                offset *= 2;
+            
+            dp[index] = dp[index - offset] + 1;
+        }
+        return dp;
+    }
+}
+
 public class Solution
 {
     public int[] countBits(int num)
