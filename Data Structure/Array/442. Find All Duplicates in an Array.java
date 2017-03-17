76f730ef7ac8a2f80cb8b8ce47dd918655346f38
Zeyu Ni <zeyuni827@gmail.com>
2017-03-17 13:55:50 -0400

    Update 442. Find All Duplicates in an Array.java

--- Data Structure/Array/442. Find All Duplicates in an Array.java
+++ Data Structure/Array/442. Find All Duplicates in an Array.java
@@ -1,18 +1,16 @@
-public class Solution
-{
-    public List<Integer> findDuplicates(int[] nums)
-    {
-        List<Integer> result = new ArrayList<>();
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if(nums == null || nums.length == 0)
+            return res;
         
-        for(int i = 0; i < nums.length; i++)
-        {
-            int val = Math.abs(nums[i]) - 1;
-            if(nums[val] > 0)
-                nums[val] = -nums[val];
+        for(int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] < 0)
+                res.add(index + 1);
             else
-                result.add(Math.abs(nums[i]));
+                nums[index] = -nums[index];
         }
-        
-        return result;
+        return res;
     }
 }
