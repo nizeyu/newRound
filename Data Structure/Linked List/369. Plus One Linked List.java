de340e74a7cb4f417c2a62add6be98c4bed07949
Zeyu Ni <zeyuni827@gmail.com>
2017-03-19 21:40:48 -0400

    Update 369. Plus One Linked List.java

--- Data Structure/Linked List/369. Plus One Linked List.java
+++ Data Structure/Linked List/369. Plus One Linked List.java
@@ -44,26 +44,31 @@ public class Solution {
  */
 public class Solution {
     public ListNode plusOne(ListNode head) {
-        if(dfs(head) == 0) {
-            return head;
-        } else {
-            ListNode newHead = new ListNode(1);
-            newHead.next = head;
-            return newHead;
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode i = dummyNode;
+        ListNode j = dummyNode;
+        
+        while(j.next != null) {
+            j = j.next;
+            if(j.val != 9)
+                i = j;
         }
-    }
-    
-    private int dfs(ListNode node) {
-        if(node == null)
-            return 1;
         
-        int carry = dfs(node.next);
+        if(j.val != 9) {
+            j.val++;
+        } else {
+            i.val++;
+            i = i.next;
+            while(i != null) {
+                i.val = 0;
+                i = i.next;
+            }
+        }
         
-        if(carry == 0)
-            return 0;
+        if(dummyNode.val == 0)
+            return dummyNode.next;
         
-        int val = node.val + 1;
-        node.val = val % 10;
-        return val / 10;
+        return dummyNode;
     }
 }
