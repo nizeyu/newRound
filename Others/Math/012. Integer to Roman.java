f47127828c83478f21c30fef0ca7885ca1c3e071
Zeyu Ni <zeyuni827@gmail.com>
2017-03-22 15:03:02 -0400

    Update 012. Integer to Roman.java

--- Others/Math/012. Integer to Roman.java
+++ Others/Math/012. Integer to Roman.java
@@ -1,26 +1,15 @@
-public class Solution
-{
-    static final int [] weight = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    static final String [] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    
-    public String intToRoman(int num)
-    {
-        int i = 0;
+public class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
         StringBuilder sb = new StringBuilder();
-        
-        while(num > 0)
-        {
-            int k = num / weight[i];
-            
-            for(int j = 0; j < k; j++)
-            {
-                sb.append(roman[i]);
-                num -= weight[i];
+        for(int i = 0; i < strs.length; ++i) {
+            while(num >= values[i]) {
+                num -= values[i];
+                sb.append(strs[i]);
             }
-            
-            i++;
         }
-        
         return sb.toString();
     }
 }
