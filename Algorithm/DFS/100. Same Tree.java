9009805cf35f949d370516499b8a1464dc93cc68
Zeyu Ni <zeyuni827@gmail.com>
2017-03-29 14:27:14 -0400

    Update 100. Same Tree.java

--- Algorithm/DFS/100. Same Tree.java
+++ Algorithm/DFS/100. Same Tree.java
@@ -24,3 +24,37 @@ public class Solution {
         
     }
 }
+
+//Non recursive Solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<TreeNode> stack_p = new Stack<>();
+        Stack<TreeNode> stack_q = new Stack<>();
+        if(p != null) stack_p.push(p);
+        if(q != null) stack_q.push(q);
+        
+        while(!stack_p.isEmpty() && !stack_q.isEmpty()) {
+            TreeNode np = stack_p.pop();
+            TreeNode nq = stack_q.pop();
+            if(np.val != nq.val) return false;
+            
+            if(np.right != null) stack_p.push(np.right);
+            if(nq.right != null) stack_q.push(nq.right);
+            if(stack_p.size() != stack_q.size()) return false;
+            
+            if(np.left != null) stack_p.push(np.left);
+            if(nq.left != null) stack_q.push(nq.left);
+            if(stack_p.size() != stack_q.size()) return false;
+        }
+        return stack_p.size() == stack_q.size();
+    }
+}
