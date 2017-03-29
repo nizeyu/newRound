1a8ac98f03a149a721e44bdc0868a30ad86e7c71
Zeyu Ni <zeyuni827@gmail.com>
2017-03-29 15:36:11 -0400

    Update 046. Permutations.java

--- Algorithm/Backtracking/046. Permutations.java
+++ Algorithm/Backtracking/046. Permutations.java
@@ -1,3 +1,33 @@
+//My Solution
+public class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return res;
+        
+        backtracking(new ArrayList<>(), new boolean[nums.length], nums);
+        
+        return res;
+    }
+    
+    private void backtracking(List<Integer> temp, boolean[] used, int[] nums) {
+        if(temp.size() == nums.length)
+            res.add(new ArrayList<>(temp));
+        else {
+            for(int i = 0; i < nums.length; ++i) {
+                if(used[i] == false) {
+                    temp.add(nums[i]);
+                    used[i] = true;
+                    backtracking(temp, used, nums);
+                    used[i] = false;
+                    temp.remove(temp.size()-1);
+                }
+            } 
+        }
+    }
+}
+
+//Discuss
 public class Solution
 {
     public List<List<Integer>> permute(int[] nums)
