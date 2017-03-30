b6f1e8cb58acb026ffb955c8530f72080d43082f
Zeyu Ni <zeyuni827@gmail.com>
2017-03-30 14:47:04 -0400

    Update 415. Add Strings.java

--- Others/Math/415. Add Strings.java
+++ Others/Math/415. Add Strings.java
@@ -1,3 +1,28 @@
+//My Solution: 3刷
+public class Solution {
+    public String addStrings(String num1, String num2) {
+        if(num1 == null || num1.length() == 0) return num2;
+        if(num2 == null || num2.length() == 0) return num1;
+        
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0 || carry == 1) {
+            int x = i >= 0 ? Character.getNumericValue(num1.charAt(i)) : 0;
+            int y = j >= 0 ? Character.getNumericValue(num2.charAt(j)) : 0;
+            int digit = x + y + carry;
+            carry = digit / 10;
+            
+            sb.insert(0, digit % 10);
+            i--;
+            j--;
+        }
+        
+        return sb.toString();
+    }
+}
+
 //My Solution: 2刷
 public class Solution {
     public String addStrings(String num1, String num2) {
