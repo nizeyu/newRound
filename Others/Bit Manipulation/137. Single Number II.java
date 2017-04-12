7259f64b3d2b13f4cb18449ae1804af779687fef
Zeyu Ni <zeyuni827@gmail.com>
2017-04-12 19:45:46 -0400

    Update 137. Single Number II.java

--- Others/Bit Manipulation/137. Single Number II.java
+++ Others/Bit Manipulation/137. Single Number II.java
@@ -1,24 +1,20 @@
-public class Solution
-{
-    public int singleNumber(int[] nums)
-    {
-        if(nums == null || nums.length == 0)
-            throw new IllegalArgumentException("Input is invalid");
+public class Solution {
+    public int singleNumber(int[] nums) {
         
-        int [] count = new int [32];
-        int result = 0;
-        
-        for(int i = 0; i < 32; i++)
-        {
-            for(int j = 0; j < nums.length; j++)
-            {
-                if(((nums[j] >> i) & 1) == 1)
-                    count[i]++;
+        int res = 0;
+        for(int i = 0; i < 32; ++i) {
+            int bit = (1 << i);
+            int count = 0;
+            for(int j = 0; j < nums.length; ++j) {
+                if((nums[j] & bit) != 0)
+                    count++;
             }
+            //res |= (count % 3 << i);
             
-            result |= ((count[i] % 3) << i);
+            if(count % 3 != 0)
+                res |= bit;
         }
         
-        return result;
+        return res;
     }
 }
