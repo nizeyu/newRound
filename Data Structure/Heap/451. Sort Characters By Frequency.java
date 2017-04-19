f5a095a84725a23556cb4d4d95c61f0bad6f5bc9
Zeyu Ni <zeyuni827@gmail.com>
2017-04-19 14:05:19 -0400

    Update 451. Sort Characters By Frequency.java

--- Data Structure/Heap/451. Sort Characters By Frequency.java
+++ Data Structure/Heap/451. Sort Characters By Frequency.java
@@ -1,25 +1,37 @@
-public class Solution
-{
-    public String frequencySort(String s)
-    {
+//My Solution
+public class Solution {
+    public String frequencySort(String s) {
         if(s == null || s.length() == 0)
             return "";
         
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < s.length(); i++)
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        Map<Character, Integer> map = new TreeMap<>();
+        for(int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
         
-        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>((a, b)->(b.getValue() - a.getValue()));
-        for(Map.Entry<Character, Integer> entry : map.entrySet())
-            maxHeap.add(entry);
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+            new Comparator<Map.Entry<Character, Integer>>() {
+                public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                    return o2.getValue() - o1.getValue();
+                }
+            }
+        );
+        
+        pq.addAll(map.entrySet());
+        
+        Comparator<Map.Entry<Character, Integer>> valueComparator = new Comparator<Map.Entry<Character, Integer>>() {
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        };
         
         StringBuilder sb = new StringBuilder();
         
-        while(!maxHeap.isEmpty())
-        {
-            Map.Entry<Character, Integer> e = maxHeap.poll();
-            for(int i = 0; i < e.getValue(); i++)
-                sb.append(e.getKey());
+        while(!pq.isEmpty()) {
+            Map.Entry entry = pq.poll();
+            for(int i = 0; i < (int)entry.getValue(); ++i)
+                sb.append(entry.getKey());
         }
         
         return sb.toString();
