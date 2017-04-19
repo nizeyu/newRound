d47b3190138c54e8b04ffb5bccb1c04103de9159
Zeyu Ni <zeyuni827@gmail.com>
2017-04-19 14:42:03 -0400

    Update 451. Sort Characters By Frequency.java

--- Data Structure/Heap/451. Sort Characters By Frequency.java
+++ Data Structure/Heap/451. Sort Characters By Frequency.java
@@ -20,12 +20,6 @@ public class Solution {
         
         pq.addAll(map.entrySet());
         
-        Comparator<Map.Entry<Character, Integer>> valueComparator = new Comparator<Map.Entry<Character, Integer>>() {
-            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
-                return o2.getValue() - o1.getValue();
-            }
-        };
-        
         StringBuilder sb = new StringBuilder();
         
         while(!pq.isEmpty()) {
