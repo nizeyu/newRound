149a51f36c6c0b712bb66e5996f8fca6ae764759
Zeyu Ni <zeyuni827@gmail.com>
2017-04-20 15:41:50 -0400

    Update 477. Total Hamming Distance.java

--- Others/Bit Manipulation/477. Total Hamming Distance.java
+++ Others/Bit Manipulation/477. Total Hamming Distance.java
@@ -1,3 +1,25 @@
+//My Solution
+public class Solution {
+    public int totalHammingDistance(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        
+        int res = 0;
+        for(int i = 0; i < 31; ++i) {
+            int mask = 1 << i;
+            
+            int count = 0;
+            for(int j = 0; j < nums.length; ++j) {
+                if((mask & nums[j]) != 0) {
+                    count++;
+                }
+            }
+            res += count * (nums.length - count);
+        }
+        return res;
+    }
+}
+
 Method 1
 public class Solution
 {
