8a6ffe9163ea5088307f5decaa8a6f7cf037bd7b
Zeyu Ni <zeyuni827@gmail.com>
2017-04-21 14:41:55 -0400

    Update 454. 4Sum II.java

--- Data Structure/Hash Table/454. 4Sum II.java
+++ Data Structure/Hash Table/454. 4Sum II.java
@@ -1,24 +1,21 @@
-public class Solution
-{
-    public int fourSumCount(int[] A, int[] B, int[] C, int[] D)
-    {
-        int result = 0;
+//Hash Table
+public class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        int res = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < A.length; i++)
-        {
-            for(int j = 0; j < B.length; j++)
-            {
-                map.put(C[i] + D[j], map.getOrDefault(C[i] + D[j], 0) + 1);
+        for(int i = 0; i < C.length; ++i) {
+            for(int j = 0; j < D.length; ++j) {
+                int sum = C[i] + D[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
             }
         }
         
-        for(int i = 0; i < A.length; i++)
-        {
-            for(int j = 0; j < B.length; j++)
-            {
-                result += map.getOrDefault(-1*(A[i] + B[j]), 0);
+        for(int i = 0; i < A.length; ++i) {
+            for(int j = 0; j < B.length; ++j) {
+                int sum = A[i] + B[j];
+                res += map.getOrDefault(-sum, 0);
             }
         }
-        return result;
+        return res;
     }
 }
