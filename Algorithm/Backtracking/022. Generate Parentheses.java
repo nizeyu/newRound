7a557c0213b4da350aae58b1f93301282255efc1
Zeyu Ni <zeyuni827@gmail.com>
2017-05-04 17:18:29 -0400

    Update 022. Generate Parentheses.java

--- Algorithm/Backtracking/022. Generate Parentheses.java
+++ Algorithm/Backtracking/022. Generate Parentheses.java
@@ -1,30 +1,26 @@
-public class Solution
-{
-    public List<String> generateParenthesis(int n)
-    {
-        List<String> result = new ArrayList<>();
-        
+//My Solution
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
         if(n <= 0)
-            return result;
+            return res;
         
-        generateParenthesis("", n, n, result);
+        backtracking(n, n, "", res);
         
-        return result;
+        return res;
     }
     
-    private void generateParenthesis(String s, int l, int r, List<String> result)
-    {
-        if(l > r)
+    private void backtracking(int i, int j, String s, List<String> res) {
+        if(i > j)//注意
             return;
         
-        if(l == 0 && r == 0)
-            result.add(s);
-        else
-        {
-            if(l > 0)
-                generateParenthesis(s + "(", l - 1, r, result);
-            if(r > 0)
-                generateParenthesis(s + ")", l, r - 1, result);
+        if(i == 0 && j == 0)
+            res.add(s);
+        else {
+            if(i > 0)
+                backtracking(i-1, j, s + '(', res);
+            if(j > 0)
+                backtracking(i, j-1, s + ')', res);
         }
     }
 }
