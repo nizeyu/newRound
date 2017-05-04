0ca9bab83bd8bdfc8c3b3f8257cdb1d768956416
Zeyu Ni <zeyuni827@gmail.com>
2017-05-04 15:48:54 -0400

    Update 216. Combination Sum III.java

--- Algorithm/Backtracking/216. Combination Sum III.java
+++ Algorithm/Backtracking/216. Combination Sum III.java
@@ -1,26 +1,57 @@
-public class Solution
-{
-    public List<List<Integer>> combinationSum3(int k, int n)
-    {
-        List<List<Integer>> result = new ArrayList<>();
+//backtracking
+public class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
         
-        combinationSum3(k, n, 1, new ArrayList<>(), result);
-        return result;
+        backtracking(k, n, 1, new ArrayList<>(), res);
+        
+        return res;
     }
     
-    private void combinationSum3(int k, int n, int start, List<Integer> tempList, List<List<Integer>> result)
-    {
-        if(n < 0)
-            return;
-        else if(tempList.size() == k && n == 0)
-            result.add(new ArrayList<>(tempList));
-        else
-        {
-            for(int i = start; i <= 9; i++)
-            {
-                tempList.add(i);
-                combinationSum3(k, n-i, i+1, tempList, result);
-                tempList.remove(tempList.size() - 1);
+    private void backtracking(int k, int n, int start, List<Integer> temp, List<List<Integer>> res) {
+        if(temp.size() == k && n == 0) {
+            res.add(new ArrayList<>(temp));
+        } else {
+            for(int i = start; i <= 9; ++i) {
+                temp.add(i);
+                backtracking(k, n-i, i+1, temp, res);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+}
+
+//wrong answer
+public class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        res = new ArrayList<>();
+        if(k < 1 || n < 1)
+            return res;
+        
+        backtracking(k, n, 1, new ArrayList<>(), new boolean[11]);
+        
+        return res;
+    }
+    
+    private void backtracking(int k, int n, int i, List<Integer> temp, boolean[] used) {
+        if(temp.size() == k) {
+            int sum = 0;
+            for(int j = 0; j < k; ++j) {
+                sum += temp.get(j);
+            }
+            //System.out.print(sum);    
+            if(sum == n)
+                res.add(new ArrayList<>(temp));
+        } else {
+            for(int j = i; j < 10; ++j) {
+                if(used[j] == false) {
+                    temp.add(j);
+                    used[j] = true;
+                    backtracking(k, n, i+1, temp, used);
+                    used[j] = false;
+                    temp.remove(temp.size()-1);
+                }
             }
         }
     }
