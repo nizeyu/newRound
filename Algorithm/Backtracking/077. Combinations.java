b6524e938f5a4e512875622bb7456e97b4a22446
Zeyu Ni <zeyuni827@gmail.com>
2017-05-09 14:38:36 -0400

    Update 077. Combinations.java

--- Algorithm/Backtracking/077. Combinations.java
+++ Algorithm/Backtracking/077. Combinations.java
@@ -1,7 +1,5 @@
-public class Solution
-{
-    public List<List<Integer>> combine(int n, int k)
-    {
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
         
         if(n <= 0 || k <= 0)
@@ -14,14 +12,11 @@ public class Solution
         return result;
     }
     
-    private void combine(int n, int k, int start, List<Integer> t, List<List<Integer>> result)
-    {
+    private void combine(int n, int k, int start, List<Integer> t, List<List<Integer>> result) {
         if(t.size() == k)
             result.add(new ArrayList<>(t));
-        else
-        {
-            for(int i = start; i <= n; i++)
-            {
+        else {
+            for(int i = start; i <= n; i++) {
                 t.add(i);
                 combine(n, k, i + 1, t, result);
                 t.remove(t.size() - 1);
