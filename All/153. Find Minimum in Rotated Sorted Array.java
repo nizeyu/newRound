cb53bd99147e54b9350b8b173218bb14b8356757
Zeyu Ni <zeyuni827@gmail.com>
2017-05-09 16:51:41 -0400

    Update

--- All/153. Find Minimum in Rotated Sorted Array.java
+++ All/153. Find Minimum in Rotated Sorted Array.java
@@ -1,15 +1,12 @@
-public class Solution
-{
-    public int findMin(int[] nums)
-    {
+public class Solution {
+    public int findMin(int[] nums) {
         if(nums == null || nums.length == 0)
             throw new IllegalArgumentException("Input is invalid");
         
         int l = 0;
         int r = nums.length - 1;
         
-        while(l < r)
-        {
+        while(l < r) {
             int m = l + (r - l) / 2;
             
             if(nums[m] < nums[r])
