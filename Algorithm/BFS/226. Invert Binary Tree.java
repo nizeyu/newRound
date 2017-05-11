aa23bd545a0cbc55a627e18c96eb5523e53f9bc6
Zeyu Ni <zeyuni827@gmail.com>
2017-05-11 14:05:05 -0400

    Update 226. Invert Binary Tree.java

--- Algorithm/BFS/226. Invert Binary Tree.java
+++ Algorithm/BFS/226. Invert Binary Tree.java
@@ -1,3 +1,37 @@
+//BFS
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+            
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            
+            if(node.left != null)
+                q.offer(node.left);
+            if(node.right != null)
+                q.offer(node.right);
+        }
+        return root;
+    }
+}
+
 //My Solution: Recursive
 /**
  * Definition for a binary tree node.
@@ -30,39 +64,4 @@ public class Solution {
     }
 }
 
-//BFS
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution
-{
-    public TreeNode invertTree(TreeNode root)
-    {
-        if(root == null)
-            return root;
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        
-        while(!q.isEmpty())
-        {
-            TreeNode node = q.poll();
-            
-            TreeNode t = node.left;
-            node.left = node.right;
-            node.right = t;
-            
-            if(node.left != null)
-                q.offer(node.left);
-            if(node.right != null)
-                q.offer(node.right);
-        }
-        return root;
-    }
-}
+
