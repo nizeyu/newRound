0f37f8e451ee3481659396cf19127998b1a32e20
Zeyu Ni <zeyuni827@gmail.com>
2017-05-11 16:49:36 -0400

    Update 455. Assign Cookies.java

--- Algorithm/Greedy/455. Assign Cookies.java
+++ Algorithm/Greedy/455. Assign Cookies.java
@@ -17,3 +17,25 @@ public class Solution
         return gg;
     }
 }
+
+//My Solution
+public class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int j = 0;
+        int count = 0;
+        for(int i = 0; i < g.length; i++) {
+            while(j < s.length && s[j] < g[i])
+                j++;
+            
+            if(j >= s.length)
+                break;
+                
+            count++;
+            j++;
+        }
+        return count;
+    }
+}
