f627d6fc83052458ff81e0ed3ac75617e7c593bd
Zeyu Ni <zeyuni827@gmail.com>
2017-05-11 17:14:30 -0400

    Update 453. Minimum Moves to Equal Array Elements.java

--- Others/Math/453. Minimum Moves to Equal Array Elements.java
+++ Others/Math/453. Minimum Moves to Equal Array Elements.java
@@ -1,19 +1,31 @@
 //My Solution
-public class Solution
-{
-    public int minMoves(int[] nums)
-    {
+public class Solution {
+    public int minMoves(int[] nums) {
         if(nums == null || nums.length == 0)
             return 0;
         
         int min = Integer.MAX_VALUE;
         int sum = 0;
-        for(int i = 0; i < nums.length; i++)
-        {
+        for(int i = 0; i < nums.length; i++) {
             sum += nums[i];
             min = Math.min(min, nums[i]);
         }
         
         return sum - min * nums.length;
+    }
+}
+
+//逆向思维
+public class Solution {
+    public int minMoves(int[] nums) {
+        int res = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++)
+            min = Math.min(min, nums[i]);
+        
+        for(int i = 0; i < nums.length; i++)
+            res += nums[i] - min;
+            
+        return res;
     }
 }
