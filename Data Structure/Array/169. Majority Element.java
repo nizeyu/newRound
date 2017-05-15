98b879105000617084f9f43fbb723a609ea8e7b4
Zeyu Ni <zeyuni827@gmail.com>
2017-05-15 09:10:08 -0400

    Update 169. Majority Element.java

--- Data Structure/Array/169. Majority Element.java
+++ Data Structure/Array/169. Majority Element.java
@@ -15,17 +15,13 @@ public class Solution {
 //Method 2： Boyer–Moore majority vote algorithm
 //https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
 
-public class Solution
-{
-    public int majorityElement(int[] nums)
-    {
+public class Solution {
+    public int majorityElement(int[] nums) {
         int count = 0;
         int result = 0;
         
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(count == 0)
-            {
+        for(int i = 0; i < nums.length; i++) {
+            if(count == 0) {
                 result = nums[i];
                 count  = 1;
             }
@@ -40,10 +36,8 @@ public class Solution
 }
 
 //Method 3
-public class Solution
-{
-    public int majorityElement(int[] nums)
-    {
+public class Solution {
+    public int majorityElement(int[] nums) {
         Arrays.sort(nums);
 
 		return nums[nums.length / 2];
