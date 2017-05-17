254f429f998192c983a2e3c57a849f03232bbfaf
Zeyu Ni <zeyuni827@gmail.com>
2017-05-16 20:00:19 -0400

    Update 144. Binary Tree Preorder Traversal.java

--- Data Structure/Tree/144. Binary Tree Preorder Traversal.java
+++ Data Structure/Tree/144. Binary Tree Preorder Traversal.java
@@ -1,4 +1,3 @@
-//My Solution
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -8,37 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if(root == null)
-            return res;
-        
-        preorder(root, res);
-        
-        return res;
-    }
-    
-    private void preorder(TreeNode node, List<Integer> res) {
-        if(node == null)
-            return;
-        
-        res.add(node.val);
-        preorder(node.left, res);
-        preorder(node.right, res);
-    }
-}
-
-//My Solution
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+//Method 0: Non-Recursion (Recommend)
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
@@ -56,6 +25,43 @@ public class Solution {
             if(node.left != null)
                 stack.push(node.left);
         }
+        return res;
+    }
+}
+
+//Method 1: Traverse
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preorder(root, res);
+        return res;
+    }
+    // 把root为根的preorder加入result里面
+    private void preorder(TreeNode node, List<Integer> res) {
+        if(node == null)
+            return;
+        
+        res.add(node.val);
+        preorder(node.left, res);
+        preorder(node.right, res);
+    }
+}
+
+//Method 2: Divide & Conquer
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null)
+            return res;
+        
+        //Divide
+        List<Integer> left = preorderTraversal(root.left);
+        List<Integer> right = preorderTraversal(root.right);
+        
+        //Conquer
+        res.add(root.val);
+        res.addAll(left);
+        res.addAll(right);
         
         return res;
     }
