cbc2c90d29be5790aba2c299dba6599c0744b259
Zeyu Ni <zeyuni827@gmail.com>
2017-05-18 10:48:20 -0400

    Create 236. Lowest Common Ancestor of a Binary Tree.java

---
+++ Data Structure/Tree/236. Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// Method: Divide & Conquer
+public class Solution {
+    // 在root为根的二叉树中找A,B的LCA:
+    // 如果找到了就返回这个LCA
+    // 如果只碰到A，就返回A
+    // 如果只碰到B，就返回B
+    // 如果都没有，就返回null
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q)
+            return root;
+        
+        //Divid
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        //Conquer
+        if(left != null && right != null)
+            return root;
+        if(left != null)
+            return left;
+        if(right != null)
+            return right;
+        return null;
+    }
+}
