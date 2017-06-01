b550dba4595d1d8e9e1abb65696cd4438e31787e
Zeyu Ni <zeyuni827@gmail.com>
2017-06-01 10:18:25 -0700

    Update 120. Triangle.java

--- Data Structure/Array/120. Triangle.java
+++ Data Structure/Array/120. Triangle.java
@@ -1,3 +1,42 @@
+//Method 1: DFS-Traverse
+public class Solution {
+    int min = Integer.MAX_VALUE;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0)
+            return 0;
+        dfs(0, 0, 0, triangle);
+        return min;
+    }
+    
+    //sum = root -> (x, y) 但不包含（x, y）的路径和
+    private void dfs(int x, int y, int sum, List<List<Integer>> triangle) {
+        if(x == triangle.size()) {
+            min = Math.min(min, sum);
+            return;
+        }
+        dfs(x+1, y, sum+triangle.get(x).get(y), triangle);
+        dfs(x+1, y+1, sum+triangle.get(x).get(y), triangle);
+    }
+}
+
+//Method 2: DFS-Divide Conquer
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0)
+            return 0;
+        
+        return divideConquer(0, 0, triangle);
+    }
+    //return minimum path from (x, y) to bottom
+    private int divideConquer(int x, int y, List<List<Integer>> triangle) {
+        if(x == triangle.size())
+            return 0;
+        
+        return triangle.get(x).get(y) + Math.min(divideConquer(x+1, y, triangle), divideConquer(x+1, y+1, triangle));
+    }
+}
+
+
 public class Solution
 {
     public int minimumTotal(List<List<Integer>> triangle)
