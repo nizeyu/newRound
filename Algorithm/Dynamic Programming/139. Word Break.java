c554c877ebabb3dabffd34522c738f6269412efa
Zeyu Ni <zeyuni827@gmail.com>
2017-06-29 16:06:06 -0700

    Update 139. Word Break.java

--- Algorithm/Dynamic Programming/139. Word Break.java
+++ Algorithm/Dynamic Programming/139. Word Break.java
@@ -1,7 +1,26 @@
-
-
-
-
+// DP
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        
+        int len = s.length();
+        
+        boolean[] dp = new boolean[len+1];
+        dp[0] = true;
+        
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+        }
+        
+        return dp[len];
+    }
+}
 
 // brute force
 public class Solution {
