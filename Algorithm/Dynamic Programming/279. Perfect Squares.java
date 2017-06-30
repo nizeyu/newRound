6e06d3a49cb3f59d9780a9d39f593d437cace77e
Zeyu Ni <zeyuni827@gmail.com>
2017-06-30 10:59:51 -0700

    Update 279. Perfect Squares.java

--- Algorithm/Dynamic Programming/279. Perfect Squares.java
+++ Algorithm/Dynamic Programming/279. Perfect Squares.java
@@ -1,18 +1,22 @@
-public class Solution
-{
-    public int numSquares(int n)
-    {
-        if(n < 0)
-            return 0;
+public class Solution {
+    public int numSquares(int n) {
         
-        int [] dp = new int[n+1];
-        
-        for(int i = 0; i < dp.length; i++)
-        {
-            dp[i] = i;
-            for(int j = 1; j * j <= i; j++)
-                dp[i] = Math.min(dp[i-j*j] + 1, dp[i]);
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        for (int j = 1; j < dp.length; j++) {
+            dp[j] = Integer.MAX_VALUE;
         }
+        
+        for (int i = 1; i * i <= n; i++) {
+            for (int j = 0; j < dp.length; j++) {
+                if (j + i * i >= dp.length) {
+                    break;
+                }
+                dp[j + i * i] = Math.min(dp[j + i * i], 1 + dp[j]);
+            }
+            
+        }
+        
         return dp[n];
     }
 }
