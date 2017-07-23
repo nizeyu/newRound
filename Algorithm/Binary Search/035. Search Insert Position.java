d0384f514ea75fb0dca0dda6f4352bed66a16b1e
Zeyu Ni <zeyuni827@gmail.com>
2017-07-22 22:58:12 -0700

    Update 035. Search Insert Position.java

--- Algorithm/Binary Search/035. Search Insert Position.java
+++ Algorithm/Binary Search/035. Search Insert Position.java
@@ -1,27 +1,23 @@
-//My Solution:
+//Self: lowerBound
 public class Solution {
     public int searchInsert(int[] nums, int target) {
-        if(nums.length == 0)
-            return 0;
-        
+        return lowerbound(nums, target);
+    }
+    
+    private int lowerbound(int[] nums, int target) {
         int l = 0;
-        int r = nums.length - 1;
-        
-        if(target < nums[l])
-            return 0;
-        if(target > nums[r])
-            return r + 1;
-        
-        while(l <= r) {
+        int r = nums.length;
+
+        while (l < r) {
             int mid = l + (r - l) / 2;
-            
-            if(target == nums[mid])
-                return mid;
-            else if(target < nums[mid])
-                r = mid - 1;
-            else
+
+            if (target <= nums[mid]) {
+                r = mid;
+            } else {
                 l = mid + 1;
-        }
-        return l;
+            }
+        } 
+
+	    return l;
     }
 }
