e6dabbd46cb3c0cac07ef61464b84546f7598301
Zeyu Ni <zeyuni827@gmail.com>
2017-09-08 10:47:07 -0700

    Update 021. Merge Two Sorted Lists.java

--- Data Structure/Linked List/021. Merge Two Sorted Lists.java
+++ Data Structure/Linked List/021. Merge Two Sorted Lists.java
@@ -1,33 +1,49 @@
-//My Solution
+//Self
 /**
- * Definition for singly-linked list.
+ * Definition for ListNode.
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
  * }
- */
+ */ 
 public class Solution {
+    /**
+     * @param ListNode l1 is the head of the linked list
+     * @param ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
+        if (l1 == null) {
+            return l2;
+        }
+        
+        if (l2 == null) {
+            return l1;
+        }
         
         ListNode dummyNode = new ListNode(0);
         ListNode p = dummyNode;
         
-        while(l1 != null && l2 != null) {
-            if(l1.val <= l2.val) {
-                p.next = new ListNode(l1.val);
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
                 l1 = l1.next;
             } else {
-                p.next = new ListNode(l2.val);
+                p.next = l2;
                 l2 = l2.next;
             }
             p = p.next;
         }
         
-        if(l1 != null) p.next = l1;
-        if(l2 != null) p.next = l2;
+        if (l1 != null) {
+            p.next = l1;
+        } else if (l2 != null) {
+            p.next = l2;
+        }
         
         return dummyNode.next;
     }
