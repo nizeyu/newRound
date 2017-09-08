ed73867776a7b334938da9de47c524fb5a65c10c
Zeyu Ni <zeyuni827@gmail.com>
2017-09-08 11:01:04 -0700

    Update 141. Linked List Cycle.java

--- Data Structure/Linked List/141. Linked List Cycle.java
+++ Data Structure/Linked List/141. Linked List Cycle.java
@@ -1,28 +1,37 @@
-//My Solution
+// Self
 /**
- * Definition for singly-linked list.
- * class ListNode {
+ * Definition for ListNode.
+ * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
  *     }
  * }
- */
+ */ 
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null)
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {  
+        if (head == null || head.next == null) {
             return false;
+        }
         
         ListNode slow = head;
         ListNode fast = head;
-        while(fast.next != null && fast.next.next != null) {
+        
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast)
+            
+            if (slow == fast) {
                 return true;
+            }
         }
+        
         return false;
     }
 }
