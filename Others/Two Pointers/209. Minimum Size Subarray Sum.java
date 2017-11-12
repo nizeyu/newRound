c3b0af684b5527bd98fbe0b71f7f6b2f67784c16
Zeyu Ni <zeyuni827@gmail.com>
2017-11-12 11:49:17 -0800

    Update 209. Minimum Size Subarray Sum.java

--- Others/Two Pointers/209. Minimum Size Subarray Sum.java
+++ Others/Two Pointers/209. Minimum Size Subarray Sum.java
@@ -1,3 +1,4 @@
+// O(n)
 class Solution {
     public int minSubArrayLen(int s, int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -22,4 +23,46 @@ class Solution {
         
         return min == Integer.MAX_VALUE ? 0 : min; 
     }
+}
+
+// O(nlog n)
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int[] sums = new int[nums.length + 1];
+        
+        for (int i = 1; i < sums.length; i++) {
+            sums[i] = sums[i-1] + nums[i-1];
+        }
+        
+        int minLen = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < sums.length; i++) {
+            int end = lowerbound(i+1, sums.length, sums[i]+s, sums);
+            if (end == sums.length) {
+                break;
+            }
+            if (end - i < minLen) {
+                minLen = end - i;
+            } 
+        }
+        
+        return minLen == Integer.MAX_VALUE ? 0 : minLen; 
+    }
+    
+    private int lowerbound(int l, int r, int target, int[] sums) {
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            
+            if (target <= sums[mid]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    } 
 }
